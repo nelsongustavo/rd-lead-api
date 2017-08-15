4b45c547068b80e7e008329723dfa9d070c31834
Nelson Gustavo <nelsongborges@gmail.com>
2017-08-15 17:30:14 -0300

    heroku settings

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20170815143513) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tracks", force: :cascade do |t|
     t.string "url"
     t.datetime "date"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tracks_on_user_id"
@@ -28,4 +31,5 @@ ActiveRecord::Schema.define(version: 20170815143513) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tracks", "users"
 end
