1b62fbbfb2e688b11239b0bfe00c8ad0dab5f59d
Nelson Gustavo <nelsongborges@gmail.com>
2017-08-16 18:11:09 -0300

    cors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -27,7 +27,7 @@ class Api::V1::UsersController < Api::V1::BaseController
   private
 
   def user_params
-    params.permit(:name, :email,  tracks_attributes: [:url, :data])
+    params.permit(:name, :email,  tracks_attributes: [:url, :date])
   end
 
   def set_user
